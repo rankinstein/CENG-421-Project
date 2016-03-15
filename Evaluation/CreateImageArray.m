06b7fa7d4b62f4ee8898cfa5d3ffc5d9624f42b8
Pravjot Rai <pravjot.s.rai@gmail.com>
2016-03-14 17:59:43 -0700

    Create array of images for testing and training [Template]
    script creates a array of testing images of  length n, with m occupied
    parking spots in the array

---
+++ Evaluation/CreateImageArray.m
@@ -0,0 +1,11 @@
+function imgs  = CreateImageArray( n, m )
+%SUMMARY: This function creates a array of images with length n which
+%consists of exactly m occupied segments
+%Input Parameters
+% n         - length of array
+% m         - number of occupied segments present in output array
+
+
+
+end
+
