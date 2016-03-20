6c18e86f189619a268f5aeedbdc5fad19f0d76eb
Pravjot Rai <pravjot.s.rai@gmail.com>
2016-03-20 00:19:05 -0700

    Created a LBP3 STILL NEEDS VALIDATION! [please validate]
    Created a LBP function that assumes that you are using a 3x3
    neighborhood, and weights matrix.   Its way faster, even when using
    nlfilter. Still requires integration into main script.

--- Local Binary Pattern/test.m
+++ Local Binary Pattern/test.m
@@ -6,47 +6,48 @@ load('boat512.mat');
 %create neighbourhood sliding window nlfilter or im2col
 camera_rotated = imrotate(camera256,30);
 tic
-func = @(x) LBP(x);
+func = @(x) LBP3(x);
 t3 = nlfilter(boat512, [3 3], func);
-t2 = nlfilter(camera256, [3 3], func);
-t1 = nlfilter(camera_rotated, [3 3], func);
+% t2 = nlfilter(camera256, [3 3], func);
+% t1 = nlfilter(camera_rotated, [3 3], func);
 
-figure; 
-subplot(1,3,1);
-imshow(camera_rotated, []);
-subplot(1,3,2);
-imshow(camera256, []);
-subplot(1,3,3);
-imshow(boat512, []);
+% figure; 
+% subplot(1,3,1);
+% imshow(camera_rotated, []);
+% subplot(1,3,2);
+% imshow(camera256, []);
+% subplot(1,3,3);
+% imshow(boat512, []);
 
 nbins = 60;
 figure;
-h1 = histogram(t1,nbins,'Normalization','probability'); 
-hold on
-h2 = histogram(t2,nbins,'Normalization','probability');
-h3 = histogram(t3,nbins,'Normalization','probability');
+% h1 = histogram(t1,nbins,'Normalization','probability'); 
+% hold on
+% h2 = histogram(t2,nbins,'Normalization','probability');
+hx = histogram(t3,nbins,'Normalization','probability');
 legend('camera256 rotated', 'camera256', 'boat512');
 toc
 %repeat 
 
 %%
 tic 
-t6 = LocalBinaryPattern(boat512, [3 3]);
-t5 = LocalBinaryPattern(camera256, [3 3]);
-t4 = LocalBinaryPattern(camera_rotated, [3 3]);
+func = @(x) LBP(x);
+t6 = nlfilter(boat512, [3 3], func);
+% t5 = nlfilter(camera256, [3 3], func);
+% t4 = nlfilter(camera_rotated, [3 3], func);
 toc
-figure; 
-subplot(1,3,1);
-imshow(camera_rotated, []);
-subplot(1,3,2);
-imshow(camera256, []);
-subplot(1,3,3);
-imshow(boat512, []);
+% figure; 
+% subplot(1,3,1);
+% imshow(camera_rotated, []);
+% subplot(1,3,2);
+% imshow(camera256, []);
+% subplot(1,3,3);
+% imshow(boat512, []);
 
 nbins = 60;
 figure;
-h1 = histogram(t4,nbins,'Normalization','probability'); 
-hold on
-h2 = histogram(t5,nbins,'Normalization','probability');
-h3 = histogram(t6,nbins,'Normalization','probability');
+% h1 = histogram(t4,nbins,'Normalization','probability'); 
+% hold on
+% h2 = histogram(t5,nbins,'Normalization','probability');
+hy = histogram(t6,nbins,'Normalization','probability');
 legend('camera256 rotated', 'camera256', 'boat512');
--- Main.m
+++ Main.m
@@ -16,13 +16,13 @@ if ~exist('file_set','var')
 end
 
 % Select a set of images from the test files
-test_data = GetImagesAndData(randsample(test_set, 50));
+test_data = GetImagesAndData(randsample(test_set, 26));
 
 % Segment the images for the training files
 training_data = GetImagesAndData(training_set);
 
 %%
-
+disp('Generating Training Data');
 % Array of only occupied spots (For training)
 training_data_occupied = training_data([training_data{:,2}] == 1,:);
 % Array of empty spots (For training)
@@ -30,7 +30,7 @@ training_data_empty = training_data([training_data{:,2}] == 0,:);
 % Array of bad spots missing data (For reference)
 training_data_bad = training_data([training_data{:,2}] == -1,:);
 
-
+disp('Generating Test Data');
 % Array of only occupied spots (For testing)
 test_data_occupied = test_data([test_data{:,2}] == 1,:);
 % Array of empty spots (For testing)
@@ -40,6 +40,7 @@ test_data_bad = test_data([test_data{:,2}] == -1,:);
 
 
 %%
+disp('Creating Local Binary Pattern Training Vectors Data');
 %Get feature vectors for each training set and Pattern
 if ~exist('LBP_features_occupied','var')
     LBP_features_occupied = FeatureVectors(training_data_occupied(:,3),'LBP');
@@ -47,24 +48,29 @@ end
 if ~exist('LBP_features_empty','var')
     LBP_features_empty = FeatureVectors(training_data_empty(:,3),'LBP');
 end
+
+disp('Creating Local Phase Quantization Training Vectors Data');
 if ~exist('LPQ_features_occupied','var')
     LPQ_features_occupied = FeatureVectors(training_data_occupied(:,3),'LPQ');
 end
 if ~exist('LPQ_features_empty','var')
     LPQ_features_empty = FeatureVectors(training_data_empty(:,3),'LPQ');
 end
-
+disp('Applying Fisher Discriminat Analysis on LBP and LPQ Training Vectors');
 %Use Fisher Discriminant Analysis to reduce dimensions between empty and occupied
 [LBP_db, LBP_V] = FisherDiscriminant(LBP_features_empty, LBP_features_occupied);
 [LPQ_db, LPQ_V] = FisherDiscriminant(LPQ_features_empty, LPQ_features_occupied);
 
+
+disp('Creating Local Binary Pattern Test Vectors Data');
 %Get feature vectors for each test set and Pattern
-% if ~exist('LBP_test_features_occupied','var')
-%     LBP_test_features_occupied = FeatureVectors(test_data_occupied(:,3),'LBP');
-% end
-% if ~exist('LBP_test_features_empty','var')
-%     LBP_test_features_empty = FeatureVectors(test_data_occupied(:,3),'LBP');
-% end
+if ~exist('LBP_test_features_occupied','var')
+    LBP_test_features_occupied = FeatureVectors(test_data_occupied(:,3),'LBP');
+end
+if ~exist('LBP_test_features_empty','var')
+    LBP_test_features_empty = FeatureVectors(test_data_occupied(:,3),'LBP');
+end
+disp('Creating Local Phase Quantization Test Vectors Data');
 if ~exist('LPQ_test_features_occupied','var')
     LPQ_test_features_occupied = FeatureVectors(test_data_occupied(:,3),'LPQ');
 end
@@ -72,15 +78,19 @@ if ~exist('LPQ_test_features_empty','var')
     LPQ_test_features_empty = FeatureVectors(test_data_empty(:,3),'LPQ');
 end
 
+disp('Applying Fisher Discriminat Analysis on LBP and LPQ Test Vectors');
+%Use Fisher Discriminant Analysis to reduce dimensions between empty and occupied
+[LBP_db, LBP_V] = FisherDiscriminant(LBP_test_features_empty, LBP_test_features_occupied);
+[LPQ_db, LPQ_V] = FisherDiscriminant(LPQ_test_features_empty, LPQ_test_features_occupied);
 
 %%  
 % Evaluate Local Binary Pattern Descriptor
-LBP_detections = DetermineOccupancy(o_imgs, 0, classifier);
-LBP_false_detections = abs(num_occupied - LBP_detections);
-LBP_rel_error = (LBP_detections - num_occupied)/ num_occupied;
-
-%%
-% Evaluate Local Phase Quantization Descriptor
-LPQ_detections = DetermineOccupancy(o_imgs, 1, classifier);
-LPQ_false_detections = abs(num_occupied - LPQ_detections);
-LPQ_rel_error = (LPQ_detections - num_occupied)/ num_occupied;
+% LBP_detections = DetermineOccupancy(o_imgs, 0, classifier);
+% LBP_false_detections = abs(num_occupied - LBP_detections);
+% LBP_rel_error = (LBP_detections - num_occupied)/ num_occupied;
+% 
+% %%
+% % Evaluate Local Phase Quantization Descriptor
+% LPQ_detections = DetermineOccupancy(o_imgs, 1, classifier);
+% LPQ_false_detections = abs(num_occupied - LPQ_detections);
+% LPQ_rel_error = (LPQ_detections - num_occupied)/ num_occupied;
